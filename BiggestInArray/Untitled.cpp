1513d6510419411d36b774432788d20fd28cb58f
Prakhar <kriptyco@gmail.com>
2021-07-03 01:59:29 +0530

    Added more programs

---
+++ BiggestInArray/Untitled.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+/**
+ * @brief      Finds the largest in given array in O(n**2) complexity.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size of array
+ *
+ * @return     Biggest value from array
+ */
+int findLargestNaive(int *arr, int size) {
+   for (int i = 0; i < size; i++) {
+      int flag = true;
+      for (int j = 0; j < size; j++) {
+         if (arr[j] > arr[i]) {
+            flag = false;
+         }
+      }
+      if (flag) {
+         return(arr[i]);
+      }
+   }
+   return(-1);
+}
+
+/**
+ * @brief      Finds the largest in O(n) time complexity.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size of array
+ *
+ * @return     Biggest value from array
+ */
+int findLargestLinear(int *arr, int size) {
+   if (size == 1) {
+      return(arr[0]);
+   }
+   int max = arr[0];
+
+   for (int i = 1; i < size; i++) {
+      if (arr[i] > max) {
+         max = arr[i];
+      }
+   }
+   return(max);
+}
+
+/**
+ * @brief      Finds the largest using reccursion in O(n).
+ *
+ * @param      arr    The arr
+ * @param[in]  start  The starting index
+ * @param[in]  stop   The last / ending index
+ *
+ * @return     Biggest value from array
+ */
+int findLargest(int *arr, int start, int stop) {
+   if (start == stop) {
+      return(arr[start]);
+   }
+   int mid = start + floor((stop - start) / 2);
+
+   int max1 = findLargest(arr, start, mid);
+   int max2 = findLargest(arr, mid + 1, stop);
+
+   if (max1 > max2) {
+      return(max1);
+   } else {
+      return(max2);
+   }
+}
+
+void printArr(int *arr, int size) {
+   for (int i = 0; i < size; i++) {
+      cout << arr[i] << " ";
+   }
+   cout << endl;
+}
+
+int main() {
+   int arr[] = { 16, 34, 33, 54, 30, 10, 1, 22, 69, 90, 88 };
+
+   // cout << findLargestLinear(arr, sizeof(arr) / sizeof(arr[0]));
+   // cout << findLargestNaive(arr, sizeof(arr) / sizeof(arr[0]));
+   cout << findLargest(arr, 0, sizeof(arr) / sizeof(arr[0]));
+   return(0);
+}
---
+++ SecondLargestInArray/Untitled.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+int findLargestLinear(int *arr, int size) {
+   if (size == 1) {
+      return(arr[0]);
+   }
+   int max = arr[0];
+
+   for (int i = 1; i < size; i++) {
+      if (arr[i] > max) {
+         max = arr[i];
+      }
+   }
+   return(max);
+}
+
+/**
+ * @brief      Finds second largest in given array in O(n**2) complexity.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size of array
+ *
+ * @return     Second largest value from array
+ */
+int findSecondLargestNaive(int *arr, int size) {
+   for (int i = 0; i < size; i++) {
+      int counter = 0;
+      for (int j = 0; j < size; j++) {
+         if (arr[j] > arr[i]) {
+            counter++;
+         }
+      }
+      if (counter == 1) {
+         return(arr[i]);
+      }
+   }
+   return(-1);
+}
+
+/**
+ * @brief      Finds second largest in O(n) time complexity by making using of
+ *             linear find largest
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size of array
+ *
+ * @return     Second largest value from array
+ */
+int findSecondLargestLinear(int *arr, int size) {
+   int largest       = findLargestLinear(arr, size);
+   int secondLargest = 0;
+
+   for (int i = 0; i < size; i++) {
+      if (arr[i] != largest) {
+         if (secondLargest == -1) {
+            secondLargest = arr[i];
+         } else if (arr[i] > secondLargest) {
+            secondLargest = arr[i];
+         }
+      }
+   }
+   return(secondLargest);
+}
+
+/**
+ * @brief      Finds a second largest in O(n) time complexity.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     Second largest value from array
+ */
+int findSecondLargest(int *arr, int size) {
+   int secondLargest = -1, largest = 0;
+
+   for (int i = 0; i < size; i++) {
+      if (arr[i] > arr[largest]) {
+         secondLargest     = largest;
+         largest = i;
+      } else if (arr[i] != arr[largest]) {
+         if (secondLargest == -1 || arr[i] > arr[secondLargest]) {
+            secondLargest = i;
+         }
+      }
+   }
+   return arr[secondLargest];
+}
+
+void printArr(int *arr, int size) {
+   for (int i = 0; i < size; i++) {
+      cout << arr[i] << " ";
+   }
+   cout << endl;
+}
+
+int main() {
+   int arr[] = { 16, 34, 33, 54, 30, 10, 1, 22, 69, 90, 88 };
+
+   cout << findSecondLargest(arr, sizeof(arr) / sizeof(arr[0]));
+   return(0);
+}
