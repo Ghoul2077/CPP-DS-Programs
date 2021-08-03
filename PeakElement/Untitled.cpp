13a46107d45e80af42e60424c8643671b254729e
Prakhar <kriptyco@gmail.com>
2021-08-04 00:17:09 +0530

    Added more programs

---
+++ PeakElement/Untitled.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <limits>
+using namespace std;
+
+/**
+ * @brief      Finds peak elements in a given array in O(n) time complexity,
+ *             peak elements are those elements in the array that are greater
+ *             than their immediate previous and immediate next element
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     Peak element encountered first
+ */
+int searchPeakElementNaive(int *arr, int size) {
+   if (arr[0] > arr[1] || size == 1) {
+      return(arr[0]);
+   }
+   for (int i = 1; i < size - 1; i++) {
+      if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+         return(arr[i]);
+      }
+   }
+   if (arr[size - 1] > arr[size - 2]) {
+      return(arr[size - 1]);
+   }
+
+   return -1;
+}
+
+/**
+ * @brief      Finds peak elements in a given array in O(logn) time complexity
+ *             by exploiting the fact that there is always one peak element
+ *             present in any given array rest is done using binary search,
+ *             peak elements are those elements in the array that are greater
+ *             than their immediate previous and immediate next element
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     Peak element encountered first
+ */
+int searchPeakElement(int *arr, int size) {
+   int low = 0, high = size;
+
+   while (low <= high) {
+      int mid = (low + high) / 2;
+      if ((mid == 0 || arr[mid - 1] < arr[mid]) &&
+          (mid == size - 1 || arr[mid + 1] < arr[mid])) {
+         return(arr[mid]);
+      }
+      if (mid > 0 && arr[mid - 1] >= arr[mid]) {
+         high = mid - 1;
+      } else {
+         low = mid + 1;
+      }
+   }
+
+   return(-1);
+}
+
+int main() {
+   int arr[] = { 15, 20, 40, 60, 80, 100, 200, 500, 1000, 1, 10 };
+   int size  = sizeof(arr) / sizeof(arr[0]);
+
+   cout << searchPeakElement(arr, size);
+   return(0);
+}
---
+++ SearchInSortedRotatedArray/Untitled.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <limits>
+using namespace std;
+
+int linearSearch(int *arr, int size, int elem) {
+   for (int i = 0; i < size; i++) {
+      if (arr[i] == elem) {
+         return(i);
+      }
+   }
+   return(-1);
+}
+
+int binarySearch(int *arr, int size, int elem) {
+   int start = 0, stop = size;
+
+   while (start <= stop) {
+      int mid = (start + stop) / 2;
+      if (arr[mid] < elem) {
+         start = mid + 1;
+      }else if (arr[mid] > elem) {
+         stop = mid - 1;
+      }else {
+         return(mid);
+      }
+   }
+   return(-1);
+}
+
+int searchInRotatedArrayNaive(int *arr, int size, int elem) {
+   return(linearSearch(arr, size, elem));
+}
+
+/**
+ * @brief      Searching in a sorted rotated array in O(logn) time complexity
+ *             by exploiting the fact that at any given time half of the array
+ *             will be completely sorted and other will be not, so find the 
+ *             range in which your number lies then find it using binary search
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ * @param[in]  elem  The element
+ *
+ * @return     Index of element if found else  -1
+ */
+int searchInRotatedArray(int *arr, int size, int elem) {
+   int mid = (size - 1) / 2;
+
+   if (arr[mid] == elem) {
+      return(mid);
+   }
+
+   if (arr[mid] > arr[0] && arr[mid] >= elem && arr[0] <= elem) {
+      return(binarySearch(arr, mid + 1, elem));
+   } else if (arr[mid] < arr[size - 1] && arr[mid] <= elem && arr[size - 1] >= elem) {
+      return(binarySearch(&arr[mid], mid + 1, elem));
+   } else {
+      int res;
+      if (arr[mid] > arr[0]) {
+         res = searchInRotatedArray(&arr[mid + 1], mid, elem);
+      } else if (arr[mid] < arr[size - 1]) {
+         res = searchInRotatedArray(arr, mid - 1, elem);
+      }
+      if (res != -1) {
+         return(res + mid + 1);
+      }
+   }
+
+   return(-1);
+}
+
+int main() {
+   int arr[] = { 15, 20, 40, 60, 80, 100, 200, 500, 1000, 1, 10 };
+   int size  = sizeof(arr) / sizeof(arr[0]);
+   int elem  = 15;
+
+   cout << searchInRotatedArray(arr, size, elem);
+   return(0);
+}
