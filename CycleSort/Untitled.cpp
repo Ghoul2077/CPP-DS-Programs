176b0cd00fe9223ca36950f59ac7304419311057
Prakhar <kriptyco@gmail.com>
2021-09-04 00:40:32 +0530

    Added more programs

---
+++ CycleSort/Untitled.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <limits>
+#include <experimental/random>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArr(int *arr, int size) {
+   for (int i = 0; i < size; i++) {
+      cout << arr[i] << "  ";
+   }
+}
+
+/**
+ * @brief      Sorting using the concept that if we chain the sorting
+ *             of elements then at sometime we are going to return
+ *             to the index we started the chain in turn creating a 
+ *             cycle. Hence using the above method we traverse the
+ *             array sorting in form of cycles. This takes O(n^2) time
+ *             complexity but this takes least amount of swaps.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     Number of swaps taken to sort the array
+ */
+int cycleSort(int *arr, int size) {
+   int writes = 0;
+
+   for (int i = 0; i < size - 1; i++) {
+      int item     = arr[i];
+      int position = i;
+
+      for (int j = i + 1; j < size; j++) {
+         if (arr[j] < item) {
+            position++;
+         }
+      }
+      if (i == position) {
+         continue;
+      }
+      while (arr[position] == item) {
+         position++;
+      }
+      swap(item, arr[position]);
+      writes++;
+
+      while (position != i) {
+         position = i;
+         for (int k = i + 1; k < size; k++) {
+            if (arr[k] < item) {
+               position++;
+            }
+         }
+         while (arr[position] == item) {
+            position++;
+         }
+         if (item != arr[position]) {
+            swap(item, arr[position]);
+            writes++;
+         }
+      }
+   }
+
+   return(writes);
+}
+
+int main() {
+   int arr[] = { 20, 50, 40, 10, 30 };
+   int size  = sizeof(arr) / sizeof(arr[0]);
+
+   cout << cycleSort(arr, size) << endl;
+   printArr(arr, size);
+
+   return(0);
+}
---
+++ MaxGuestMeetingPossible/Untitled.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <limits>
+#include <experimental/random>
+#include <bits/stdc++.h>
+using namespace std;
+
+int min(int num1, int num2) {
+   return(num1 > num2 ? num2 : num1);
+}
+
+int max(int num1, int num2) {
+   return(num1 > num2 ? num1 : num2);
+}
+
+/**
+ * @brief      Finds the maximum number of guests whom meeting is possible given
+ *             their arrival time array and their respective departure time
+ *             array in O(nlogn) time by using merge method of merge sort as
+ *             base.
+ *
+ * @param      arrival    The arrival
+ * @param      departure  The departure
+ * @param[in]  size       The size
+ *
+ * @return     Max number of guests that is possible for us to meet
+ */
+int maxGuestsWhomMeetingPossible(int *arrival, int *departure, int size) {
+   sort(arrival, arrival + size);
+   sort(departure, departure + size);
+
+   int maxCount = INT_MIN;
+   int count = 0;
+   int i = 0, j = 0;
+
+   while (i < size && j < size) {
+      if (arrival[i] <= departure[j]) {
+         count++;
+         i++;
+      } else {
+         count--;
+         j++;
+      }
+      maxCount = max(count, maxCount);
+   }
+
+   return(maxCount);
+}
+
+int main() {
+   int arrival[]   = { 900, 600, 700 };
+   int departure[] = { 1000, 800, 730 };
+   int size        = sizeof(arrival) / sizeof(arrival[0]);
+
+   int maxGuests = maxGuestsWhomMeetingPossible(arrival, departure, size);
+
+   cout << maxGuests;
+
+   return(0);
+}
