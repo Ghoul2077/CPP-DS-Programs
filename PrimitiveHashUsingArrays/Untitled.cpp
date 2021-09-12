e667b45d1bb25c304c6275f05c9a0d1b8ec4338d
Prakhar <kriptyco@gmail.com>
2021-09-12 17:02:29 +0530

    Added more programs

---
+++ PrimitiveHashUsingArrays/Untitled.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void insert(bool *arr, int val) {
+   arr[val] = true;
+}
+
+void search(bool *arr, int val) {
+   cout << (arr[val] ? "Found" : "Not Found") << endl;
+}
+
+void _delete(bool *arr, int val) {
+   arr[val] = false;
+}
+
+int main() {
+   bool *hashArray = new bool[1000];
+
+   insert(hashArray, 10);
+   insert(hashArray, 20);
+   insert(hashArray, 119);
+   search(hashArray, 10);
+   search(hashArray, 20);
+   _delete(hashArray, 119);
+   search(hashArray, 119);
+
+   return(0);
+}
