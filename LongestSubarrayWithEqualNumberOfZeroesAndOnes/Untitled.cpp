fce9b812d7ba0ef54acf5d9ea90a123ab71e62c5
Prakhar <kriptyco@gmail.com>
2021-09-21 21:35:07 +0530

    Added more programs

---
+++ LongestSubarrayWithEqualNumberOfZeroesAndOnes/Untitled.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief      Finds the longest subarray with equal number of zeroes and ones
+ *             O(n^2) time complexity and Q(1) space complexity.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     Max length subarray with equal zeroes and ones
+ */
+int longestSubarrayWithEqualNumberOfZeroesAndOnesNaive(int *arr, int size) {
+   int maxLength = 0;
+
+   for (int i = 0; i < size; i++) {
+      int onesCount = 0, zeroesCount = 0;
+
+      for (int j = i; j < size; j++) {
+         if (arr[j] == 0) {
+            zeroesCount++;
+         } else {
+            onesCount++;
+         }
+
+         if (zeroesCount == onesCount) {
+            maxLength = max(maxLength, zeroesCount * 2);
+         }
+      }
+   }
+
+   return (maxLength);
+}
+
+/**
+ * @brief      Finds the longest subarray with equal number of zeroes and ones
+ *             Q(n) time complexity and Q(1) space complexity.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     Max length subarray with equal zeroes and ones
+ */
+int longestSubarrayWithEqualNumberOfZeroesAndOnes(int *arr, int size) {
+   int longestSubArraySize = 0, prefixSum = 0;
+   unordered_map<int, int> sumToArraySizeMap;
+
+   for (int i = 0; i < size; i++) {
+      if (arr[i] == 0) {
+         arr[i] = -1;
+      }
+   }
+
+   for (int i = 0; i < size; i++) {
+      prefixSum += arr[i];
+
+      if (prefixSum == 0) {
+         longestSubArraySize = max(i + 1, longestSubArraySize);
+      }
+
+      if (sumToArraySizeMap.count(prefixSum) == 1) {
+         longestSubArraySize =
+            max(longestSubArraySize, i - sumToArraySizeMap[prefixSum]);
+      }
+
+      if (sumToArraySizeMap.count(prefixSum) == 0) {
+         sumToArraySizeMap.insert({ prefixSum, i });
+      }
+   }
+
+   return (longestSubArraySize);
+}
+
+int main() {
+   int arr[] = { 0, 1, 1, 0, 1, 1, 1, 0 };
+   int size  = sizeof(arr) / sizeof(arr[0]);
+
+   cout << longestSubarrayWithEqualNumberOfZeroesAndOnes(arr, size);
+   return (0);
+}
---
+++ MaxCandiesPossibleToStoreWithTwoBooksAsDam/Untitled.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief      Given array of books with height as values of the array and we
+ *             need to find the maximum candies we can store with just two books
+ *             used as dam. Time complexity is Q(n) and space complexity is Q(1)
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     { description_of_the_return_value }
+ */
+int maxCandiesPossibleToStoreWithTwoBooks(int *arr, int size) {
+   int maxCandies = 0;
+   int left = 0, right = size - 1;
+
+   while (left < right) {
+      int width          = right - left - 1;
+      int height         = min(arr[left], arr[right]);
+      int currMaxCandies = width * height;
+
+      maxCandies = max(maxCandies, currMaxCandies);
+
+      if (arr[left] < arr[right]) {
+         left++;
+      } else {
+         right--;
+      }
+   }
+
+   return (maxCandies);
+}
+
+int main() {
+   int arr[] = { 2, 1, 5, 4, 6, 5 };
+   int size  = sizeof(arr) / sizeof(arr[0]);
+
+   cout << maxCandiesPossibleToStoreWithTwoBooks(arr, size);
+   return (0);
+}
