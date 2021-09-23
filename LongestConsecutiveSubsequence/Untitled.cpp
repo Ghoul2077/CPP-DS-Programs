c7a2ca51ddab40da45372607dfb1aa50a7dbec90
Prakhar <kriptyco@gmail.com>
2021-09-23 10:32:12 +0530

    Added more programs

---
+++ LongestConsecutiveSubsequence/Untitled.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief      Finds the size of longest consecutive subsequence, a subsequence
+ *             is like x, x + 1, x + 2...x + n no matter the order they occure.
+ *             It's time complexity is O(nlogn) and space complexity is Q(1).
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     Size of longest subsequence
+ */
+int longestConsecutiveSubsequenceNaive(int *arr, int size) {
+   sort(arr, arr + size);
+   int currSize = 1, maxSize = 1;
+
+   for (int i = 1; i < size; i++) {
+      if (arr[i] == arr[i - 1] + 1)  {
+         currSize++;
+      } else if (arr[i] != arr[i - 1]) {
+         maxSize  = max(maxSize, currSize);
+         currSize = 1;
+      }
+   }
+
+   return (maxSize);
+}
+
+/**
+ * @brief      Finds the size of longest consecutive subsequence, a subsequence
+ *             is like x, x + 1, x + 2...x + n no matter the order they occure.
+ *             It's time complexity is O(2n) and space complexity is O(n).
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     Size of longest subsequence
+ */
+int longestConsecutiveSubsequence(int *arr, int size) {
+   int maxCount = 1;
+   unordered_set<int> arrayValuesSet(arr, arr + size);
+
+   for (int i = 0; i < size; i++) {
+      int count = 0;
+      int curr  = arr[i];
+
+      if (arrayValuesSet.count(arr[i] - 1) == 0) {
+         while (true) {
+            if (arrayValuesSet.count(curr++)) {
+               count++;
+            } else {
+               break;
+            }
+         }
+      }
+
+      maxCount = max(maxCount, count);
+   }
+
+   return (maxCount);
+}
+
+int main() {
+   int arr[] = { 1, 9, 3, 4, 2, 20 };
+   int size  = sizeof(arr) / sizeof(arr[0]);
+
+   cout << longestConsecutiveSubsequence(arr, size);
+   return (0);
+}
---
+++ LongestSubarrayWithCommonSum/Untitled.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief      Finds longest subarray in 2 given boolean arrays of equal sizes
+ *             given the condition that the sizes of subarrays in both array
+ *             should be same. Time complexity is O(n^2) and space complexity
+ *             is Q(1).
+ *
+ * @param      arr1  The arr 1
+ * @param      arr2  The arr 2
+ * @param[in]  size  The size
+ *
+ * @return     Size of longest subarray
+ */
+int longestSubarrayWithCommonSumNaive(int *arr1, int *arr2, int size) {
+   int maxLen = 0;
+
+   for (int i = 0; i < size; i++) {
+      int sum1 = 0, sum2 = 0;
+
+      for (int j = i; j < size; j++) {
+         sum1 += arr1[j];
+         sum2 += arr2[j];
+
+         if (sum1 == sum2) {
+            maxLen = max(maxLen, j);
+         }
+      }
+   }
+
+   return (maxLen);
+}
+
+/**
+ * @brief      Finds longest subarray in 2 given boolean arrays of equal sizes
+ *             given the condition that the sizes of subarrays in both array
+ *             should be same. In this approach we are converting our problem
+ *             to longest subarray with sum 0 by subtracting the 2 arrays.
+ *             Time complexity is Q(n) and space complexity is Q(n).
+ *
+ * @param      arr1  The arr 1
+ * @param      arr2  The arr 2
+ * @param[in]  size  The size
+ *
+ * @return     Size of longest subarray
+ */
+int longestSubarrayWithCommonSum(int *arr1, int *arr2, int size) {
+   int *newArr    = new int[size * 2];
+   int  maxLen    = 0;
+   int  prefixSum = 0;
+   unordered_map<int, int> sumToLengthMap;
+
+   for (int i = 0; i < size; i++) {
+      newArr[i] = arr1[i] - arr2[i];
+   }
+
+   for (int i = 0; i < size; i++) {
+      prefixSum += newArr[i];
+
+      if (prefixSum == 0) {
+         maxLen = max(maxLen, i + 1);
+      }
+
+      if (sumToLengthMap.count(prefixSum) == 0) {
+         sumToLengthMap.insert({ prefixSum, i });
+      } else {
+         maxLen = max(maxLen, i - sumToLengthMap[prefixSum]);
+      }
+   }
+
+   return maxLen;
+}
+
+int main() {
+   int arr1[] = { 0, 0, 1, 0 };
+   int arr2[] = { 1, 1, 1, 1 };
+   int size   = sizeof(arr2) / sizeof(arr2[0]);
+
+   cout << longestSubarrayWithCommonSum(arr1, arr2, size);
+   return (0);
+}
