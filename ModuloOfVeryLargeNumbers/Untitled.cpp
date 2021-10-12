c0ee015092816cf9ce9c7cbc0246e34df90e9b24
Prakhar <kriptyco@gmail.com>
2021-10-12 09:01:34 +0530

    Added more programs

---
+++ ModuloOfVeryLargeNumbers/Untitled.cpp
@@ -0,0 +1,43 @@
+#include <limits.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long unsigned int bigInt;
+#define mod 1000000007;
+
+/**
+ * @brief      Finds a modulo of given number with k. The trick is that the
+ *             number is given as string and will be way bigger than 10^9 so we
+ *             need to find an efficient way to solve this problem. For this we
+ *             have used rolling remainder trick which says that if we know
+ *             remainder of previous digit to left then we can calculate next
+ *             remainder by doing (prevRemainder * 10 + newDigit) % k hence
+ *             bringing time complexity down to O(n).
+ *
+ * @param      str   The string
+ * @param[in]  k     The number to get modulo by
+ *
+ * @return     Remainder
+ */
+int findModuloK(string& str, int k) {
+   if (k == 0) {
+      return (-1);
+   }
+
+   int last_remainder = 0;
+
+   for (bigInt i = 0; i < str.size(); i++) {
+      int currNum = stoi(str.substr(i, 1));
+      last_remainder = (last_remainder * 10 + currNum) % k;
+   }
+
+   return (last_remainder);
+}
+
+int main() {
+   string str = "987688594028123102937589768503827491";
+   int    k   = 11;
+
+   cout << findModuloK(str, k);
+   return (0);
+}
---
+++ PrintAllPrimeNumbersLessThanEqualToK(Prime Sieve)/Untitled.cpp
@@ -0,0 +1,75 @@
+#include <limits.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long unsigned int bigInt;
+#define mod 1000000007;
+
+bool isPrime(int num) {
+   if (num == 1) {
+      return (false);
+   }
+
+   if ((num == 2) || (num == 3)) {
+      return (true);
+   }
+
+   if ((num % 2 == 0) || (num % 3 == 0)) {
+      return (false);
+   }
+
+   for (int i = 5; i < pow(num, 0.5); i += 6) {
+      if ((num % i == 0) || (num % (i + 2) == 0)) {
+         return (false);
+      }
+   }
+
+   return (true);
+}
+
+void printAllPrimeNumbersLessThanKNaive(int k) {
+   for (int i = 1; i < k; i++) {
+      if (isPrime(i)) {
+         cout << i << " ";
+      }
+   }
+}
+
+void printallprimenumberslessthank(int k) {
+   bool *arr = new bool[k + 1];
+
+   fill(arr, arr + k + 1, true);
+
+   for (int i = 4; i < k + 1; i += 2) {
+      arr[i] = false;
+   }
+
+   for (int i = 6; i < k + 1; i += 3) {
+      arr[i] = false;
+   }
+
+   for (int i = 10; i < k + 1; i += 5) {
+      arr[i] = false;
+   }
+
+   for(int i = 0; i < k + 1; i++) {
+        if(arr[i]) {
+            cout << i << " ";
+        }
+   }
+}
+
+int main() {
+   int testCases;
+
+   cin >> testCases;
+   cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+   for (int i = 0; i < testCases; i++) {
+      int val;
+      cin >> val;
+      printAllPrimeNumbersLessThanKNaive(val);
+      cout << endl;
+   }
+   return (0);
+}
