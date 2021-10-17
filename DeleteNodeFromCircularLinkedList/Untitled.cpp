6fb113d5af2cfb9122e6724150d9c2a643091670
Prakhar <kriptyco@gmail.com>
2021-10-17 19:15:17 +0530

    Fixed some programs

--- DeleteNodeFromCircularLinkedList/Untitled.cpp
+++ DeleteNodeFromCircularLinkedList/Untitled.cpp
@@ -50,24 +50,57 @@ void printCLL(Node *head) {
    } while (currNode != head);
 }
 
+/**
+ * @brief      Deletes Node from circular linked list in O(n) time complexity
+ *             and Q(1) space complexity.
+ *
+ * @param      head  The head
+ *
+ * @return     New head of the linked list
+ */
 Node* deleteAtBeginning(Node *head) {
    Node *temp = head;
    Node *curr = head;
 
-   while(curr->next != head) {
+   while (curr->next != head) {
       curr = curr->next;
    }
 
    curr->next = head->next;
-   head = head->next;
+   head       = head->next;
+
+   delete(temp);
+   return (head);
+}
+
+/**
+ * @brief      Deletes Node from circular linked list in Q(1) time complexity
+ *             and Q(1) space complexity.
+ *
+ * @param      head  The head
+ *
+ * @return     New head of the linked list
+ */
+Node* deleteAtBeginningEfficient(Node *head) {
+   if (head == NULL) {
+      return (head);
+   }
+   Node *temp = head->next;
+   head->data = head->next->data;
+   head->next = head->next->next;
 
    delete(temp);
    return (head);
 }
 
 Node* deleteNodeAtIndex(Node *head, int index) {
+   if (index < 0) {
+      cout << "Index underflow" << endl;
+      return (head);
+   }
+
    if (index == 0) {
-      return (deleteAtBeginning(head));
+      return (deleteAtBeginningEfficient(head));
    }
 
    Node *curr  = head;
