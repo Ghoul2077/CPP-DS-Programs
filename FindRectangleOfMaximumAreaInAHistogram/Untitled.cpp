1449d95ebe3da7198b320be85b25051f14b09281
Prakhar <kriptyco@gmail.com>
2021-11-06 23:20:26 +0530

    Added more programs

---
+++ FindRectangleOfMaximumAreaInAHistogram/Untitled.cpp
@@ -0,0 +1,131 @@
+#include <limits.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long unsigned int bigInt;
+#define mod 1000000007;
+
+/**
+ * @brief      Gets the maximum area of rectangle if given an array is treated
+ *             as bars of histogram. It's time complexity is O(n^2) and space
+ *             complexity is Q(1).
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     The maximum area of rectangle.
+ */
+int getMaximumAreaOfRectangleNaive(int *arr, int size) {
+   int maxArea = INT_MIN;
+
+   for(int i = 0; i < size; i++) {
+      int currMin = arr[i];
+      for(int j = i; j < size; j++) {
+         currMin = min(currMin, arr[j]);
+         int currArea = currMin * (j - i + 1);
+         maxArea = max(maxArea, currArea);
+      }
+   }
+
+   return maxArea;
+}
+
+/**
+ * @brief      Gets the maximum area of rectangle if given an array is treated
+ *             as bars of histogram. It's time complexity is O(n) and space
+ *             complexity is Q(n). The drawback of this approach is that it
+ *             uses 4 individual loops and 2 vectors.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     The maximum area of rectangle.
+ */
+int getMaximumAreaOfRectangle(int *arr, int size) {
+   stack<int> st1, st2;
+   vector<int> arr1, arr2;
+   int maxArea = INT_MIN;
+
+   for(int i = 0; i < size; i++) {
+      while(!st1.empty() && arr[st1.top()] >= arr[i]) {
+         st1.pop();
+      }
+      int smallerElemIndex = st1.empty() ? -1 : st1.top();
+      arr1.push_back(smallerElemIndex);
+      st1.push(i);
+   }
+
+   for(int i = size - 1; i >= 0; i--) {
+      while(!st2.empty() && arr[st2.top()] >= arr[i]) {
+         st2.pop();
+      }
+      int smallerElemIndex = st2.empty() ? size : st2.top();
+      arr2.push_back(smallerElemIndex);
+      st2.push(i);
+   }
+
+   reverse(arr2.begin(), arr2.end());
+
+   for(int i = 0; i < size; i++) {
+      int currVal = arr[i];
+      int interval = arr2[i] - arr1[i] - 1;
+      int currArea = currVal * interval;
+      maxArea = max(maxArea, currArea);
+   }
+
+   return maxArea;
+}
+
+/**
+ * @brief      Gets the maximum area of rectangle if given an array is treated
+ *             as bars of histogram. It's time complexity is O(n) and space
+ *             complexity is Q(n). This is the most optimized approach as it
+ *             only uses one stack and traverses the array only once.
+ *
+ * @param      arr   The arr
+ * @param[in]  size  The size
+ *
+ * @return     The maximum area of rectangle.
+ */
+int getMaximumAreaOfRectangleOptimized(int *arr, int size) {
+   stack<int> st;
+   int maxArea = INT_MIN;
+
+   for(int i = 0; i < size; i++) {
+      while(!st.empty() && arr[st.top()] >= arr[i]) {
+         int top = st.top();
+         st.pop();
+         int curr = arr[top] * (st.empty() ? i : (i - st.top() - 1));
+         maxArea = max(maxArea, curr);
+      }
+      st.push(i);
+   }
+
+   while(!st.empty()) {
+      int top = st.top();
+      st.pop();
+      int curr = arr[top] * (st.empty() ? size : size - st.top() - 1);
+      maxArea = max(maxArea, curr);
+   }
+
+   return maxArea;
+}
+
+int main() {
+   int testCases;
+
+   cin >> testCases;
+   cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+   for (int i = 0; i < testCases; i++) {
+      int size;
+      cin >> size;
+      int *arr = new int[size];
+      for(int i = 0; i < size; i++) {
+         cin >> arr[i];
+      }
+      cout << getMaximumAreaOfRectangleOptimized(arr, size);
+      cout << endl;
+   }
+   return (0);
+}
---
+++ LargestRectangleWithAllOnesInBooleanMatrix/Untitled.cpp
@@ -0,0 +1,119 @@
+#include <limits.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long unsigned int bigInt;
+#define mod 1000000007;
+
+/**
+ * @brief      Finds largest area rectangle with all ones when given a boolean
+ *             matrix. It's time compleixty is O((RC)^3) and space complexity is
+ *             Q(1).
+ *
+ * @param      arr   The arr
+ * @param[in]  rows  The rows
+ * @param[in]  cols  The cols
+ *
+ * @return     Rectangle of largest area
+ */
+int findLargestRectangleWithAllOnesNaive(int **arr, int rows, int cols) {
+   int largestAreaOfRectangle = 0;
+
+   for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+         for(int k = i; k < rows; k++) {
+            for(int l = j; l < cols; l++) {
+               bool areAllOnes = true;
+               for(int m = i; m <= k; m++) {
+                  for(int n = j; n <= l; n++) {
+                     if(arr[m][n] == 0) {
+                        areAllOnes = false;
+                        break;
+                     }
+                  }
+               }
+               if(areAllOnes) {
+                  int currAreaOfRectangle = (k - i + 1) * (l - j + 1);
+                  largestAreaOfRectangle = max(largestAreaOfRectangle, currAreaOfRectangle);
+               }
+            }
+         }
+      }
+   }
+
+   return largestAreaOfRectangle;
+}
+
+int rectangleOfMaxArea(int *arr, int size) {
+   stack<int> st;
+   int maxArea = 0;
+
+   for(int i = 0; i < size; i++) {
+      while(!st.empty() && arr[st.top()] >= arr[i]) {
+         int top = st.top();
+         st.pop();
+         int currSize = arr[top] * (st.empty() ? i : (i - st.top() - 1));
+         maxArea = max(maxArea, currSize);
+      }
+      st.push(i);
+   }
+
+   while(!st.empty()) {
+      int top = st.top();
+      st.pop();
+      int currSize = arr[top] * (st.empty() ? size : (size - st.top() - 1));
+      maxArea = max(maxArea, currSize);
+   }
+
+   return maxArea;
+}
+
+/**
+ * @brief      Finds largest rectangle with all ones for a given boolean matrix
+ *             . This approach is an extension of max area of rectangle in
+ *             histogram. It's time complexity is Q(R * C) and space complexity
+ *             is Q(C).
+ *
+ * @param      arr   The arr
+ * @param[in]  rows  The rows
+ * @param[in]  cols  The cols
+ *
+ * @return     Rectangle with largest area
+ */
+int findLargestRectangleWithAllOnes(int **arr, int rows, int cols) {
+   int res = rectangleOfMaxArea(arr[0], cols);
+
+   for(int i = 1; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+         if(arr[i][j] == 1) {
+            arr[i][j] += arr[i - 1][j];
+         }
+      }
+      res = max(res, rectangleOfMaxArea(arr[i], cols));
+   }
+
+   return res;
+}
+
+int main() {
+   int testCases;
+
+   cin >> testCases;
+   cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+   for (int i = 0; i < testCases; i++) {
+      int rows, cols;
+      cin >> rows;
+      cin >> cols;
+      int **arr = new int*[rows];
+      for(int i = 0; i < rows; i++) {
+         arr[i] = new int[cols];
+         for(int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+         }
+      }
+      cout << findLargestRectangleWithAllOnes(arr, rows, cols);
+      cout << endl;
+   }
+   return (0);
+}
