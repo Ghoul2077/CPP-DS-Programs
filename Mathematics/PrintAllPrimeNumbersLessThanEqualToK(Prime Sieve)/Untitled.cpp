499239237a32b79692aedecf8b4e562940c7c1a7
Prakhar <kriptyco@gmail.com>
2023-07-07 23:14:10 +0530

    Fix prime sieve algorithm

--- Mathematics/PrintAllPrimeNumbersLessThanEqualToK(Prime Sieve)/Untitled.cpp
+++ Mathematics/PrintAllPrimeNumbersLessThanEqualToK(Prime Sieve)/Untitled.cpp
@@ -5,70 +5,60 @@ typedef long long unsigned int bigInt;
 #define mod 1000000007;
 
 bool isPrime(int num) {
-   if (num == 1) {
-      return (false);
-   }
+    if (num == 1) {
+        return (false);
+    }
 
-   if ((num == 2) || (num == 3)) {
-      return (true);
-   }
+    if ((num == 2) || (num == 3)) {
+        return (true);
+    }
 
-   if ((num % 2 == 0) || (num % 3 == 0)) {
-      return (false);
-   }
+    if ((num % 2 == 0) || (num % 3 == 0)) {
+        return (false);
+    }
 
-   for (int i = 5; i < pow(num, 0.5); i += 6) {
-      if ((num % i == 0) || (num % (i + 2) == 0)) {
-         return (false);
-      }
-   }
+    for (int i = 5; i < pow(num, 0.5); i += 6) {
+        if ((num % i == 0) || (num % (i + 2) == 0)) {
+            return (false);
+        }
+    }
 
-   return (true);
+    return (true);
 }
 
 void printAllPrimeNumbersLessThanKNaive(int k) {
-   for (int i = 1; i < k; i++) {
-      if (isPrime(i)) {
-         cout << i << " ";
-      }
-   }
+    for (int i = 1; i < k; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
 }
 
-void printallprimenumberslessthank(int k) {
-   bool *arr = new bool[k + 1];
+void printAllPrimeNumbersLessThanK(int k) {
+    bool *arr = new bool[k + 1];
+    fill(arr, arr + k + 1, false);
 
-   fill(arr, arr + k + 1, true);
-
-   for (int i = 4; i < k + 1; i += 2) {
-      arr[i] = false;
-   }
-
-   for (int i = 6; i < k + 1; i += 3) {
-      arr[i] = false;
-   }
-
-   for (int i = 10; i < k + 1; i += 5) {
-      arr[i] = false;
-   }
-
-   for (int i = 0; i < k + 1; i++) {
-      if (arr[i]) {
-         cout << i << " ";
-      }
-   }
+    for (int i = 0; i * i < k; i++) {
+        if (arr[i] == true) {
+            cout << i << " ";
+            for (int j = 2 * i; j < k + 1; j += i) {
+                arr[i] = false;
+            }
+        }
+    }
 }
 
 int main() {
-   int testCases;
+    int testCases;
 
-   cin >> testCases;
-   cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    cin >> testCases;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-   for (int i = 0; i < testCases; i++) {
-      int val;
-      cin >> val;
-      printAllPrimeNumbersLessThanKNaive(val);
-      cout << endl;
-   }
-   return (0);
+    for (int i = 0; i < testCases; i++) {
+        int val;
+        cin >> val;
+        printAllPrimeNumbersLessThanKNaive(val);
+        cout << endl;
+    }
+    return (0);
 }
